97830d1fcc7131628be338518fb206a335f6f937
Tarmo Sillajõe <tarmos@autoparts.local>
2019-11-14 10:24:41 +0200

    Steps working now: - opening the url - clicking on "OE Numbers & Linked Vehicles"

--- trw_scraper.py
+++ trw_scraper.py
@@ -6,6 +6,16 @@ driver = Chrome(webdriver)
 
 url = "https://www.trwaftermarket.com/en/catalogue/product/GDB5005/"
 driver.get(url)
-product_details_link = driver.find_element_by_xpath('*[@id="catalogue-search-results"]/div[contains(@class, "product-details")]/a[contains(@href, "en/catalogue/product/GDB5005/")]')
-product_details_link.click()
+# product_details_link = driver.find_element_by_xpath('*[@id="catalogue-search-results"]/div[contains(@class, "product-details")]/a[contains(@href, "en/catalogue/product/GDB5005/")]')
+# product_details_link.click()
+oe_refs = driver.find_element_by_xpath('//*[@id="tabs"]//a[text()="OE Numbers & Linked Vehicles"]')
+oe_refs.click()
+# product_oe_refs_link = driver.find_element_by_link_text("OE Numbers & Linked Vehicles")
+# product_oe_refs_link.click()
 #driver.close()
+
+
+# //*[@id="ui-id-30"]
+
+# 'a[contains(@class, "ui-tabs-anchor")]'
+# "OE Numbers & Linked Vehicles"
