f75ed16281dd3c5625bc7e6691754748fc4bce8b
Tarmo Sillajõe <tarmos@autoparts.local>
2019-11-14 12:26:05 +0200

    This now works: - opening oe-table - reading oe application and oe code from table rows.

--- trw_scraper.py
+++ trw_scraper.py
@@ -1,8 +1,10 @@
 from selenium.webdriver import Chrome
 from pathlib import Path
 
+
 webdriver= Path.cwd() / "driver" / "chromedriver.exe"
 driver = Chrome(webdriver)
+driver.maximize_window()
 
 url = "https://www.trwaftermarket.com/en/catalogue/product/GDB5005/"
 driver.get(url)
@@ -11,5 +13,12 @@ driver.get(url)
 oe_refs = driver.find_element_by_xpath('//*[@id="tabs"]//a[text()="OE Numbers & Linked Vehicles"]')
 oe_refs.click()
 
-oe_accordion_header = driver.find_element_by_xpath('//div[@id="oe-numbers-accordion"]//h3')
-oe_accordion_header.click()
+driver.find_element_by_xpath('//div[@id="oe-numbers-accordion"]/h3').click()
+oe_table= driver.find_element_by_xpath('//div[@id="oe-numbers-accordion"]//table[@class="responsive"]')
+oe_table_rows = oe_table.find_elements_by_tag_name('tr')
+
+oe_list = []
+for row in oe_table_rows[1:]:
+    print(row.find_element_by_xpath('td[1]').text + ' '+ row.find_element_by_xpath('td[2]').text )
+
+driver.close()
