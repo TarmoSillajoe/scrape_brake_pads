938bd772782e405ed30fe13812902c13c6b3c1dc
Tarmo Sillajõe <tarmos@autoparts.local>
2019-11-15 14:15:57 +0200

    modified:   trw_scraper.py In TrwScraper class added method to check if page exists. The method returns true if response's status code is 200. Redirecting (301) results false.

--- draft.py
+++ draft.py
@@ -1,24 +1,20 @@
 from trw_scraper import TrwScraper
 import csv
 import pandas as pd
-
+import requests
 
 #GDB5008
 scraper = TrwScraper()
 
 scraped_data = []
 
-products_list = [
-    'GDB5019'
-    ,'GDB5008'
-    ,'GDB5005'
-                ]
+df_parts = pd.read_csv('part_numbers.csv')
+print(df_parts.columns)
 
-for product in products_list:
+for product in df_parts['part_number'].unique():
     product_data = scraper.scrape_product_data(product)
     scraped_data += product_data
 
 
 df = pd.DataFrame(scraped_data)
 df.to_csv('C:/Users/tarmos/Downloads/trw.csv', sep='\t', index=False)
-
--- trw_scraper.py
+++ trw_scraper.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium.webdriver import Firefox, FirefoxOptions
 from pathlib import Path
+import requests
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -22,20 +23,20 @@ class TrwScraper():
 
     
     def scrape_product_data(self, product_number):
-
+        print(f'scraping {product_number}')
         url = f"https://www.trwaftermarket.com/en/catalogue/product/{product_number}/"
+
+        oe_list = []
+        if not self.page_exists(url): return [{'product_number':product_number ,'make':None, 'oe_number':None}]
+
         self.driver.get(url)
         self.driver.implicitly_wait(20)
 
         WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="tabs"]//a[text()="OE Numbers & Linked Vehicles"]'))).click()
-        #self.driver.find_element_by_xpath('//*[@id="tabs"]//a[text()="OE Numbers & Linked Vehicles"]').click()
-
         WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//div[@id="oe-numbers-accordion"]//h3'))).click()
-        #self.driver.find_element_by_xpath('//div[@id="oe-numbers-accordion"]//h3').click()
         oe_table= self.driver.find_element_by_xpath('//div[@id="oe-numbers-accordion"]//table[@class="responsive"]')
         oe_table_rows = oe_table.find_elements_by_tag_name('tr')
 
-        oe_list = []
 
         if len(oe_table_rows) < 2: return [{'product_number':product_number ,'make':None, 'oe_number':None}]
 
@@ -47,6 +48,12 @@ class TrwScraper():
             oe_list.append(oe_dict)
 
         return oe_list
+
+
+    def page_exists(self, url):
+        request = requests.get(url, allow_redirects=False)  
+        if request.status_code == 200: return True
+        return False
 
 
     def __del__(self):
